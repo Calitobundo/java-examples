e739fef95791eab4595fdc5930238cd5289086f5
calito <calitobundo@mail.de>
2020-05-18 02:52:58 +0200

    Updated

--- src/main/java/timer/TimerEvent.java
+++
@@ -1,8 +0,0 @@
-package timer;
-
-@FunctionalInterface
-public interface TimerEvent {
-
-    void onTimer(long time);
-
-}
--- src/main/java/timer/TimerExample.java
+++ src/main/java/timer/TimerExample.java
@@ -2,52 +2,70 @@ package timer;
 
 public class TimerExample {
 
+    public static void main(String[] args) throws InterruptedException {
+        new TimerExample();
+    }
 
-    private Thread thread;
+    public TimerExample() throws InterruptedException {
 
-    public TimerExample() {
+        //TimerCallback callback = now -> {
+        //    System.out.println("now: " + now);
+        //};
+
+        TimerCallback callback2 = new MyCallback();
+
+        Timer timer =  new Timer(2000, callback2);
+        timer.start();
+
+        Thread.sleep(8000);
+        timer.stop();
 
     }
 
-    public static void main(String[] args) {
-        TimerExample app = new TimerExample();
-        app.start();
-    }
-
-    public void start(){
-
-        thread =  new TimerThread(now -> {
-            System.out.println("now: "+now);
-        });
-        thread.start();
-
-    }
-
-
-    public class TimerThread extends Thread {
-
-        private final TimerEvent event;
-        private long before;
-
-        public TimerThread(TimerEvent event) {
-            this.event = event;
-            this.before = System.currentTimeMillis();
-        }
+    public class MyCallback implements TimerCallback {
 
         @Override
-        public void run() {
-
-            while(true) {
-
-                long now = System.currentTimeMillis();
-                long delta = now - before;
-                if(delta > 2000){
-                    before = now;
-                    event.onTimer(now);
-                }
-
-            }
+        public void onTimer(long now) {
+            System.out.println("now: " + now);
         }
+    }
+
+
+    public class Timer {
+
+        private Thread thread;
+        private volatile boolean running = true;
+
+        private long before;
+
+        public Timer(long callbackTime, TimerCallback callback) {
+            this.before = System.currentTimeMillis();
+            this.thread = new Thread(() -> {
+
+                while(running) {
+                    long now = System.currentTimeMillis();
+                    long delta = now - before;
+                    if(delta > callbackTime){
+                        callback.onTimer(now);
+                        before = now;
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException ignored) {
+                    }
+                }
+            });
+
+        }
+
+        public void start(){
+            thread.start();
+        }
+
+        public void stop(){
+            running = false;
+        }
+
     }
 
 }
